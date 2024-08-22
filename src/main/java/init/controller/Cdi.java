824b9d2752fce9d3b5fb8f47313f7e790227de59
IgorHenM <igormori16@gmail.com>
2024-08-22 14:13:38 -0300

    feat: Adicionando classe de conexão com api (FinanceClient), classe de cálculo (Cdi) e classe de geração de resumo de cálculo

--- src/main/java/init/controller/Cdi.java
+++ src/main/java/init/controller/Cdi.java
@@ -1,13 +1,21 @@
 package init.controller;
 
-import java.util.ArrayList;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+/**
+ * This class its necessary to all Cdi functions: calculate month cash yield, calculate total month cash amounth, total cash yield, total cash investment and total cash amount.
+ */
 
 public class Cdi {
 
     private double initialInvestment;
     private int yearsOfInvestment;
-    private final double tax = 11.68 / 12 / 100;
+    private double tax = 0;
     private double monthInvest;
+    private final NumberFormat nf = NumberFormat.getCurrencyInstance();
+    private final FinanceClient fc = new FinanceClient();
 
     public Cdi() {
         this(0, 0, 0);
@@ -17,12 +25,17 @@ public class Cdi {
         this.initialInvestment = initialInvestment;
         this.yearsOfInvestment = yearsOfInvestment;
         this.monthInvest = monthInvest;
+        this.tax = fc.getCdiTax() / 12 / 100;
     }
 
     public double getInitialInvestment() {
         return initialInvestment;
     }
-
+    
+     /**
+      * This method its necessary to all calculations because represent the initial investment
+      * @param initialInvestment Initial investment
+      */
     public void setInitialInvestment(double initialInvestment) {
         this.initialInvestment = initialInvestment;
     }
@@ -30,9 +43,17 @@ public class Cdi {
     public int getYearsOfInvestment() {
         return yearsOfInvestment;
     }
+    /**
+     * This method its necessary to all calculations because represent the years of investment
+     * @param yearsOfInvestment The investment time (years)
+     */
 
     public void setYearsOfInvestment(int yearsOfInvestment) {
         this.yearsOfInvestment = yearsOfInvestment;
+    }
+
+    public void setTax() {
+        this.tax = fc.getCdiTax() / 12 / 100;
     }
 
     public double getTax() {
@@ -53,7 +74,7 @@ public class Cdi {
 
         for (int i = 1; i <= months; i++) {
             if (i == 1) {
-                total += getInitialInvestment() * getTax();
+                total += total * getTax();
             } else {
                 total += getMonthInvest();
                 total += total * getTax();
@@ -77,23 +98,69 @@ public class Cdi {
         return getTotal() - getTotalInvest();
     }
 
-    public ArrayList<Double> getMonthRend() {
+    public String[][] getResultInfos() {
+        double total = getInitialInvestment();
+        double totalMonth = getInitialInvestment();
+        double initialMonth = getInitialInvestment();
+        double rend = 0;
+        int months = getYearsOfInvestment() * 12;
+        String[][] infos = new String[months][3];
+
+        for (int i = 0; i < months; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 0) {
+                    if (j == 0) {
+                        infos[i][j] = "1º";
+                    } else if (j == 1) {
+                        infos[i][j] = nf.format(getInitialInvestment() * getTax());
+                    } else {
+                        infos[i][j] = nf.format(initialMonth + (initialMonth * getTax()));
+                        total = initialMonth + (initialMonth * getTax());
+                        totalMonth = initialMonth + (initialMonth * getTax());
+                    }
+                } else {
+                    if (j == 0) {
+                        infos[i][j] = (i + 1) + "º";
+                    } else if (j == 1) {
+                        total += getMonthInvest();
+                        rend = total * getTax();
+                        infos[i][j] = nf.format(rend);
+                    } else {
+                        totalMonth += getMonthInvest();
+                        totalMonth += totalMonth * getTax();
+                        infos[i][j] = nf.format(totalMonth);
+                    }
+                }
+
+            }
+        }
+        return infos;
+    }
+
+    public String fileContent() {
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+
+        DateTimeFormatter formDate = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter formTime = DateTimeFormatter.ofPattern("HH-mm-ss");
+
+        String fileContent = "Informações da Simulação:\nData: " + formDate.format(date) + "\nHora: " + formTime.format(time) + "\n\nMensal: \n";
+
         double total = getInitialInvestment();
         int months = getYearsOfInvestment() * 12;
-        ArrayList<Double> listOfRend = new ArrayList<>();
 
         for (int i = 1; i <= months; i++) {
             if (i == 1) {
-                total += total * getTax();
-                listOfRend.add(total);
+                total += getInitialInvestment() * getTax();
+                fileContent += "\n" + i + "º Mês:" + nf.format(total);
             } else {
                 total += getMonthInvest();
                 total += total * getTax();
-                listOfRend.add(total);
+                fileContent += "\n" + i + "º Mês:" + nf.format(total);
             }
         }
-        return listOfRend;
+        fileContent += "\n\nTotal investido: " + nf.format(getTotalInvest()) + "\nRendimento Total: " + nf.format(getRend()) + "\nMontante total: " + nf.format(getTotal());
+        return fileContent;
     }
 
-    
 }
--- src/main/java/init/controller/Recibo.java
+++ src/main/java/init/controller/Recibo.java
@@ -1,19 +1,27 @@
 package init.controller;
 
+import java.awt.Desktop;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import javax.swing.JOptionPane;
+
 public class Recibo {
 
-    private Path path = Path.of("src\\main\\files");
+    private Path path = Path.of("C:\\cdi_calculations\\");
     private String fileContent;
     private final DateTimeFormatter dateAndHour = DateTimeFormatter.ofPattern("HH-mm-ss_ddMMyyyy");
 
     public Recibo() {
+        this("");
+    }
 
+    public Recibo(String fileContent) {
+        this.fileContent = fileContent;
     }
 
     public String getFileContent() {
@@ -28,11 +36,20 @@ public class Recibo {
         return path;
     }
 
+    public void createLocal() {
+        File directory = new File(getPath().toString());
+        if (!directory.exists()) {
+            if (directory.mkdirs()) {
+                JOptionPane.showMessageDialog(null, "diretório criado.");
+            }
+        }
+    }
+
     public void createFile() {
         LocalDateTime date = LocalDateTime.now();
         String dateHour = dateAndHour.format(date);
 
-        path = Path.of("src\\main\\files\\" + dateHour + ".txt");
+        path = Path.of("C:\\cdi_calculations\\" + dateHour + ".txt");
         try {
             Files.createFile(path);
         } catch (IOException e) {
@@ -45,10 +62,19 @@ public class Recibo {
         }
     }
 
-    public void insertContent() {
+    public void openLocal() {
+        File directory = new File("C:\\cdi_calculations");
 
+        if (Desktop.isDesktopSupported()) {
+            Desktop computer = Desktop.getDesktop();
+            try {
+                computer.open(directory);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Not supported.");
+        }
     }
-
-    
 
 }
